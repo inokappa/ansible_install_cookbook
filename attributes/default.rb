58baddee34f68b3c60c508225db7107bc2239350
kappa <kappa@x1carbon.inokara.com>
2013-06-23 13:33:00 +0900

    fixed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,6 +9,7 @@ default['epel']['repo_url_path'] = 'http://ftp.riken.jp/Linux/fedora/epel/'
 default['epel']['file_name'] = "epel-release-#{default['epel']['ver_num']}.noarch.rpm"
 default['epel']['uri_path'] = "#{default['epel']['repo_url_path']}" + "/" + "#{default['epel']['os_ver_num']}" + "/" + "#{default['epel']['arch']}" + "/" + "#{default['epel']['file_name']}"
 #
+default['epel']['repo_dir_path'] = '/etc/yum.repos.d/'
 default['epel']['repo_file_path'] = '/etc/yum.repos.d/epel.repo'
 # 
 default['ansible']['install_path'] = '/usr/bin/ansible'
--- recipes/ansible.rb
+++ recipes/ansible.rb
@@ -3,13 +3,22 @@ remote_file node['epel']['work_dir'] + node['epel']['file_name'] do
   not_if {File.exists?(node['epel']['work_dir'] + node['epel']['file_name'])}
 end
 
-execute "install epel" do
+execute "install_epel" do
   command "rpm -Uvh 'node['epel']['work_dir'] + node['epel']['file_name']'"
   not_if {File.exists?(node['epel']['repo_file_path'])}
 end
 
+bash "enable_epel" do
+  user "root"
+  cwd node['epel']['repo_dir_path']
+  code <<-EOH
+    sed -i 's/enabled=0/enabled=1/g' #{node['epel']['repo_file_path']} && \
+    touch #{node['epel']['repo_dir_path']}/eple.enabled
+  EOH
+  #not_if {File.exists?(node['epel']['repo_dir_path']/eple.enabled)}
+end
+
 package "ansible" do
   action :install
-  not_if {File.exists?(node['ansible']['install_path'])}
 end
 
